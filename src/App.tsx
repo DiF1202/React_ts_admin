344328a90f947d99d243eae444d4dfd06f8ff09b
hedifei <hedifei@corp.netease.com>
2022-10-25 12:36:01 +0800

    feat: 1

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          Learn React
+          Learn Reac
         </a>
       </header>
     </div>
